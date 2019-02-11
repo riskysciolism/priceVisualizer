7bbc261f3841ca45c96a5c9318d9a993a088697c
riskysciolism <richard.musebrink@gmail.com>
2019-02-11 14:28:19 +0100

    CHANGED Trying to convert to socket rather than RESTful

---
+++ controller/categoryController.js
@@ -0,0 +1,22 @@
+const category = require('../models/categoryModel');
+
+function createCategory(request, response, next) {
+    console.log("Add category");
+    console.log(request.body);
+    
+    let requestData = request.body;
+    let newCategory = new category(requestData);
+    
+    newCategory.save().then(data => response.json(data)).catch(next);
+}
+
+function getCategories(request, response, next) {
+    category.find().then(data => {
+        response.json(data);
+    });
+}
+
+module.exports = {
+    createCategory: createCategory,
+    getCategories: getCategories
+};
---
+++ controller/itemController.js
@@ -0,0 +1,68 @@
+const item = require('../models/itemModel');
+
+function getItems() {
+    item.find().then(data => 
+        {
+        return JSON(data);
+        }
+    );
+}
+
+function getItemPrices(name) {
+    item.findOne({name: name}).then(data => {
+        let labels = [];
+        let itemPrices = [];
+
+        for(let i = 0; i < data.price.length; i++) {
+            itemPrices.push(data.price[i].value);
+            labels.push(data.price[i].timestamp);
+        }
+        
+        let payload = {
+            labels: labels,
+            datasets:[{
+                label: data.name,
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: itemPrices
+            }]
+        };
+
+        return payload;
+    })
+}
+
+
+async function createItem(request, response, next) {
+    console.log("Add Item");
+    console.log(request.body.price);
+
+    let requestData = request.body;
+    let newItem = new item({
+        name: requestData.name,
+        description: requestData.description,
+        category: requestData.category,
+        price: [{
+            value: requestData.price
+        }]
+    });
+    
+    await newItem.save().then(data => {
+        response.json(data)
+    });
+}
+
+function updatePrice(name, price) {
+    item.findOneAndUpdate(
+        {name: name},
+        {
+            $push: { 'item.price.value': price }
+        },
+        { safe: true }).exec().catch(error => console.log(error));
+}
+
+module.exports = {
+    getItems: getItems,
+    getItemPrices: getItemPrices,
+    createItem: createItem
+};
