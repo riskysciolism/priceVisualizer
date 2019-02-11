63177921951c7135ac30ce1c1ff75e32a2aef5da
riskysciolism <richard.musebrink@gmail.com>
2019-02-11 14:27:28 +0100

    CHANGED Trying to convert to socket rather than RESTful

--- index.js
+++ index.js
@@ -4,14 +4,45 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-const items = require('./controller/itemController');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const item = require('./controller/itemController')
 
-const item = require('./routes/api/item');
+/*const item = require('./routes/api/item'),
+      category = require('./routes/api/category');
+*/
 
-app.use(express.static(__dirname + '/public'));
-app.use('/api/item', item);
+module.exports = app;
 
+console.log('Node running..');
+
+configureExpress();
 configureDatabase();
+
+
+function configureExpress() { 
+  app.use(express.static(path.join(__dirname, 'public')));
+  /*app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+      next();
+  });
+
+  // Use body parser so we can get info from POST and/or URL parameters.
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json({limit: "8mb"}));
+
+  // Use morgan to log requests to the console.
+  app.use(morgan('dev'));
+
+  //app.use(express.static(__dirname + '/public'));
+  app.use('/api/item', item);
+  app.use('/api/category', category);
+  */
+  console.log('Express configured..');
+}
 
 function configureDatabase(){
   mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
@@ -26,7 +57,8 @@ function configureDatabase(){
 }
 
 function onConnection(socket){
-  //socket.emit('connected', data);
+  console.log("first: " + item.getItemPrices('Water'));
+  socket.emit('connected', item.getItemPrices('Water'));
   console.log("Socket connected..");
 }
 
--- routes/api/item.js
+++
@@ -1,8 +0,0 @@
-let router = require('express').Router(),
-    items = require('../../controller/itemController');
-
-router.route('/')
-    .get(items.getItemPrices)
-    .post(items.createItem);
-
-module.exports = router;
